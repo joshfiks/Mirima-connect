3d6169162d9fcf86dc423aef289ca140214e3b43
joshfiks <joshfiks04@gmail.com>
2026-08-05 21:11:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1090,6 +1090,37 @@ showLoading("Submitting Your Feedback...", () => {
 }, btn);
 
   });
+
+
+  // ==========================================
+// GREETING
+// ==========================================
+
+function updateGreeting(){
+
+    const hour = new Date().getHours();
+
+    let greeting = "";
+
+    if(hour < 12){
+
+        greeting = "☀️ Good Morning";
+
+    }else if(hour < 18){
+
+        greeting = "🌿 Good Afternoon";
+
+    }else{
+
+        greeting = "🌙 Good Evening";
+
+    }
+
+    document.getElementById("weatherTemp").textContent = greeting;
+
+}
+
+updateGreeting();
   
 // ==========================================
 // LIVE CLOCK
