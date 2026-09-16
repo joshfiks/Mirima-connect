9480116ad6be98cb47d286eff3a7efa3d6d5315b
joshfiks <joshfiks04@gmail.com>
2026-09-16 10:12:04 +0300

    Add Mirima Connect Firebase configuration
    Added Firebase configuration settings including apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, and measurementId.

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,10 @@
-// Mirima Firebase configuration
-// Firebase project settings will be added here.
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCqrDXZicyekk7wU4l86iWxkXEZ9UVa_SM",
+  authDomain: "mirima-d8e41.firebaseapp.com",
+  projectId: "mirima-d8e41",
+  storageBucket: "mirima-d8e41.firebasestorage.app",
+  messagingSenderId: "110084294772",
+  appId: "1:110084294772:web:24d6d07b509ad740cae921",
+  measurementId: "G-3THXS3WC79"
 };
