93b70c68f6cc32775f79fd9d4d16878adc4ccd36
joshfiks <joshfiks04@gmail.com>
2026-09-16 11:23:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { firebaseConfig } from "./firebase-config.js";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const app = initializeApp(firebaseConfig);
 // ==========================================
