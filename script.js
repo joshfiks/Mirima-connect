42bf863c8551a0ba87f0a89a8affe40c969e89bf
joshfiks <joshfiks04@gmail.com>
2026-09-16 12:07:30 +0300

    Remove temporary Firestore test
    Removed Firestore write operation for 'test' collection.

--- script.js
+++ script.js
@@ -11,17 +11,6 @@ import {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-addDoc(collection(db, "test"), {
-    message: "Mirima Firestore test",
-    createdAt: serverTimestamp()
-})
-    .then(function () {
-        console.log("Mirima Firestore write successful");
-    })
-    .catch(function (error) {
-        console.error("Mirima Firestore write failed:", error);
-    });
-
 // ==========================================
 // MIRIMA CONNECT
 // Main Script
